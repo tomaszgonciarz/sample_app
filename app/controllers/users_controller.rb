69132de55aab1cec5c44085d70dab1d93bf5b9cc
Tomek Gonciarz <tomas.gonciarz@gmail.com>
2019-08-11 21:20:19 +0200

    Add index to user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: %i[edit update]
+  before_action :logged_in_user, only: %i[edit update index]
   before_action :set_user, only: %i[show edit update]
   before_action :correct_user, only: %i[edit update]
+
+  def index
+    @users = User.all
+  end
 
   def new
     @user = User.new
